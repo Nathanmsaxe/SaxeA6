b9824d00b96d3314f1b5b694146c5629fe2f2263
Nathanmsaxe <59581764+Nathanmsaxe@users.noreply.github.com>
2022-04-20 20:35:38 -0700

    Update String.cpp

--- String.cpp
+++ String.cpp
@@ -1,3 +1,9 @@
+/*
+ * String.cpp
+ *
+ *  Created on: Apr 20, 2022
+ *      Author: student
+ */
 #include "String.h"
 #include <cstring>
 #include <iostream>
@@ -14,33 +20,38 @@ String::String() {
 
 }
 String::String(const String& rhs){
+	str = new char(SIZE);
+	this->len = strlen(rhs.str);
 	strcpy(this->str, rhs.str);
 	this->len = rhs.len;
-	str[(rhs.len)+1] = '\0';
+	str[len] = '\0';
 }
 String::String(const char charArr[]){
-	while(charArr[this->len]!=nullptr){
-		this->str[this->len] = charArr[this->len];
-		this->len++;
+	str = new char(SIZE);
+	this->len = strlen(charArr);
+	for(unsigned int i = 0; i <= this->len; i++){
+		this->str[i] = charArr[i];
 	}
+	str[len] = '\0';
+
 
 }
-String String::operator=(const char rhs[]){
-	while(rhs[this->len]!=nullptr){
-		this->str[this->len] = rhs[this->len];
-		this->len++;
+String String::operator=(const char rhs[]){ //seems good
+	this->len = strlen(rhs);
+	for(unsigned int i = 0; i < strlen(rhs); i++){
+		this->str[i] = rhs[i];
 	}
 	return *this;
 
 }
-String String::operator=(String rhs){
+String String::operator=(String& rhs){
 	strcpy(this->str, rhs.str);
 	this->len = rhs.len;
-	str[(rhs.len)+1] = '\0';
+	str[this->len+1] = '\0';
 
 	return *this;
 }
-char String::operator[](int pos){
+char& String::operator[](int pos){
 	return this->str[pos];
 }
 bool String::operator==(String rhs){
@@ -48,7 +59,7 @@ bool String::operator==(String rhs){
 		return false;
 	}
 	else{
-		for(int i = 0; i< this->len; i++){
+		for(unsigned int i = 0; i< this->len; i++){
 			if(this->str[i]!=rhs.str[i]){
 				return false;
 			}
@@ -57,13 +68,32 @@ bool String::operator==(String rhs){
 	return true;
 }
 bool String::operator==(const char rhs[]){
-	strcpy()
+	if(strlen(rhs)>this->len || strlen(rhs)>this->len){
+			return false;
+	}
+	else{
+		for(unsigned int i = 0; i< this->len; i++){
+			if(this->str[i]!=rhs[i]){
+				return false;
+			}
+		}
+	}
+	return true;
 }
 int String::length(){
-	return len;
+	return this->len;
+}
+ostream& operator<< (ostream & os, String& m){ //seems good
+	for(unsigned int i = 0 ; i < m.len; i++){
+		os << m.str[i];
+	}
+	return os;
 }
 
 String::~String() {
 	// TODO Auto-generated destructor stub
 	delete[] str;
 }
+
+
+
